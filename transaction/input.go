ecd3a0343de6105d2f60ecc130276e71e295e845
Tobias <tobias.aditya@gmail.com>
2022-06-12 22:10:51 +0700

    fix model input transaction

--- transaction/input.go
+++ transaction/input.go
@@ -6,6 +6,6 @@ type InputTransaction struct {
 	TrxType string `json:"trxType" binding:"required"`
 	Amount  string `json:"amount" binding:"required"`
 	Desc    string `json:"desc" binding:"required"`
-	Method  string `json:"method" binding:"required"`
+	Method  string `json:"trxMethod" binding:"required"`
 	User    user.User
 }
