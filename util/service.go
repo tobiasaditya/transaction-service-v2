67bb903f69f2ec0002929d6ae52771f65badd68b
Tobias <tobias.aditya@gmail.com>
2023-01-07 22:03:01 +0700

    fix variable timezone, don't know why can't pick up env variable

--- util/service.go
+++ util/service.go
@@ -1,19 +1,20 @@
 package util
 
 import (
-	"os"
 	"time"
 )
 
 func CTimeNow() time.Time {
-	timezone := os.Getenv("TZ")
+	// timezone := os.Getenv("TZ")
+	timezone := "Asia/Jakarta"
 	loc, _ := time.LoadLocation(timezone)
 	return time.Now().In(loc)
 
 }
 
 func FormatTime(input time.Time) string {
-	timezone := os.Getenv("TZ")
+	// timezone := os.Getenv("TZ")
+	timezone := "Asia/Jakarta"
 	loc, _ := time.LoadLocation(timezone)
 	return input.In(loc).Format("2006-01-02 15:04:05")
 
